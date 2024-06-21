2e9c90953f5bf4adf06072ce9ba24f894197e6fb
Simon Linke <135662449+Simon-Linke@users.noreply.github.com>
2024-06-21 22:46:35 +0200

    Update IPF_functions.py

--- Python/IPF_functions.py
+++ Python/IPF_functions.py
@@ -3,9 +3,9 @@ import numpy as np
 def IPF(g,p,g0):
     # calculates the IPF a single time
 
-    # g is array with previous n system state
-    # p is array of IPF modelling paramters \alpha and \beta_k
-    # g0 is inital value, wich is used, when IPF is diverging
+    # g is an array with the previous n system states
+    # p is an array of IPF modeling parameters \alpha and \beta_k
+    # g0 is an initial value used when IPF is diverging
     g1=np.array(g)
     g1[g1!=g1]=g0 #replace nan values by g0
 
@@ -26,9 +26,9 @@ def IPF(g,p,g0):
 def IPF_ser(it,p,g0):
     # calculates the IPF multiple iterations
 
-    # it numner of iterations of calculating the IPF
+    # it number of iterations of calculating the IPF
     # p is array of IPF modelling paramters \alpha and \beta_k
-    # g0 is inital value, wich is used, when IPF is diverging
+    # g0 is the initial value, which is used when IPF is diverging
     g=np.ones(len(p))*g0
 
     G=np.zeros(it)
@@ -42,10 +42,10 @@ def IPF_ser(it,p,g0):
 def IPF_chAlpha(it,p,al,g0):
     # calculates the IPF multiple iterations while linearly changing alpha
 
-    # it numner of iterations of calculating the IPF
+    # it number of iterations of calculating the IPF
     # p is array of IPF modelling paramters alpha (at start) and \beta_k
     # al alpha after all iterations
-    # g0 is inital value, wich is used, when IPF is diverging
+    # g0 is the initial value, which is used when IPF is diverging
 
     alp=np.linspace(p[0], al, it)
     g=np.ones(len(p))*g0
@@ -58,7 +58,7 @@ def IPF_chAlpha(it,p,al,g0):
     return(G)
 
 def ampMod(A,c,han=True):
-    #weights the Amplitude of a single Periode with the factor c, if han=true the Amplitude is also weighted by a hanning-window
+    #weights the Amplitude of a single Periode with the factor c. If han=true, the Amplitude is also weighted by a hanning-window
     A=np.array(A)
     if han:
         A=A*np.hanning(len(A))
